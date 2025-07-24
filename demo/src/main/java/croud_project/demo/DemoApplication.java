dbc84f9c254cfb5d7fee01407e43a2fcb190ebf5
yuna2321 <yuna1021@dgu.ac.kr>
2025-07-24 13:13:21 +0900

    git push

---
+++ demo/src/main/java/croud_project/demo/DemoApplication.java
@@ -0,0 +1,11 @@
+package croud_project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}

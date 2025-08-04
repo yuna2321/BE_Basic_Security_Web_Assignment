9358f22744ed33813ed8a051474f7947dcd6d39e
yuna2321 <yuna1021@dgu.ac.kr>
2025-08-05 00:30:13 +0900

    [feat] - 리포지토리와 스프링부트 연동

---
+++ src/main/java/crud/crudPractice/CrudPracticeApplication.java
@@ -0,0 +1,13 @@
+package crud.crudPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPracticeApplication.class, args);
+	}
+
+}
